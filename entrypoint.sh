a1510f4e92ebc2074a7dbf86505994b803f7cea9
Matt Rendina <mrendina@stsci.edu>
2020-03-20 17:52:01 -0400

    Change input var name; pass input to validation script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
-echo "This is the string provided: $1"
+git_tag="${1}"
+echo "This is the string provided: ${git_tag}"
 
 PYTHON=$(which python3)
-$PYTHON /validate_version.py
+$PYTHON /validate_version.py ${git_tag}
 
 time=$(date)
 echo "::set-output name=time::$time"
-
 
--- validate_version.py
+++ validate_version.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
 
-print('validation script output')
+import sys
+print(f'Validation script argument: {sys.argv[1]}')
