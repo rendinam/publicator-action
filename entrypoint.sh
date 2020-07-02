8f7bfc4dfa787dcbef152d3f9f71c92739fffdc1
Matt Rendina <mrendina@stsci.edu>
2020-07-02 17:33:01 -0400

    Try consolidating all into custom action

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,22 @@ pwd
 REF=$GITHUB_REF
 echo "This is the string from GITHUB_REF env var: ${REF}"
 
+echo $(which python)
+echo $(python --version)
 PYTHON=$(which python3)
 $PYTHON /validate_version.py ${REF}
 
 time=$(date)
+
+echo "Install package"
+pip install .
+
+echo "Install publication deps"
+pip install twine
+
+#echo "Prepare for publication 1"
+#git clean -fxd
+
 echo "::set-output name=time::$time"
 echo "--Entrypoint end"
 
