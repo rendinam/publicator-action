0cedba4daed162f784fe9f84ff5742621fdf38b9
Matt Rendina <mrendina@stsci.edu>
2020-07-02 13:12:05 -0400

    Try accessing GITHUB_REF env var directly in entrypoint script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -l
 
 git_tag="${1}"
-echo "This is the string provided: ${git_tag}"
+echo "This is the string provided as argument: ${git_tag}"
+
+echo "This is the string from GITHUB_REF env var: ${GITHUB_REF}"
 
 PYTHON=$(which python3)
 $PYTHON /validate_version.py ${git_tag}
