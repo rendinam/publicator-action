e7d42fbf1c592325578c0bffec232a1c83e97b95
Matt Rendina <mrendina@stsci.edu>
2020-07-07 13:45:19 -0400

    Stop upon entrypoint script failure. Doc update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
+set -x
+
 echo "--Entrypoint begin"
 echo "Custom working directory: $(pwd)"
 ls -al
--- validate_version.py
+++ validate_version.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
+# Perform a simple check on the tag portion of the git reference
+# passed in as the first argument.:bn
 
 import sys
-import os
 import semver
 
-print('*****************************')
-print('*****************************')
+print('\n')
 gitref = sys.argv[1]
 print(f'Validation script argument: {gitref}')
 tag = gitref.split('/')[-1]
 print(f'Tag: {tag}')
 version = semver.VersionInfo.parse(tag)
 print('Tag is a valid semver value.')
-print('*****************************')
-print('*****************************')
